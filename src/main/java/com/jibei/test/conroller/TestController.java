e6a6e3132925e0dc1e66fc1c46f9ff991cd7be63
fish <13087788+a-chinese-fish@user.noreply.gitee.com>
2023-06-20 11:52:05 +0800

    jenkins v2.0.0

--- src/main/java/com/jibei/test/conroller/TestController.java
+++ src/main/java/com/jibei/test/conroller/TestController.java
@@ -8,7 +8,7 @@ public class TestController {
 
     @GetMapping("/test")
     public String test(){
-        return "Hello Jenkins! --- v1.0.0";
+        return "Hello Jenkins! --- v2.0.0";
     }
 
 }
